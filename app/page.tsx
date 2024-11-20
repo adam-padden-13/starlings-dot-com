cbdf4f3ed072ba4dca8a38eeaa73f446848e0819
adam-padden-13 <paddenadam@gmail.com>
2024-11-20 09:44:24 -0500

    fixed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         </div>
       </header>
       <main className="flex flex-col row-start-2 items-center ">
-        <div className="flex gap-4 items-center flex-col sm:flex-row mt-24">
+        <div className="flex gap-4 items-center  sm:flex-row mt-24">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://tr.ee/PvzvgXxp9U"
